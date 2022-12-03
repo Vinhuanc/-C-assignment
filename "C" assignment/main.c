0fa2fde056a62d77d62ddbbf7b04911e78b2c464
Vinhuanc <huann.chenn@gmail.com>
2022-12-03 09:39:04 -0600

    Initial Commit

---
+++ "C" assignment/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  "C" assignment
+//
+//  Created by Chen, Huan on 12/3/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
